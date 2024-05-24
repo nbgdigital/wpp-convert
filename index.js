d397f27f65b038622385623c3d40b369e44eb185
gerencia.dev@nbgdigital.com.br <flavioc.buarque@gmail.com>
2024-05-23 23:55:55 -0300

    check

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Importar o Fastify
 const fastify = require('fastify')({ logger: false });
 
+// Checar disponibilidade
+fastify.get('/check', async (request, reply) => {
+    reply.code(200).send({ message: 'Success!' });
+})
+
 // Definir a rota POST
 fastify.post('/message', async (request, reply) => {
     // Extrair o corpo da requisição (JSON enviado pelo cliente)
@@ -22,14 +27,13 @@ fastify.post('/message', async (request, reply) => {
     const name = splitName[0];
 
     // Codificar a mensagem em URL
-    const encodedMessage = `Olá, meu nome é ${name} e vim pelo instagram!\n\n*Nome completo*: ${fullName}\nPlano: ${plan}\nData de vencimento: Dia ${dueDate}.\n\nTelefone pra contato: ${phone}\nEndereço: ${street}, número ${number}, ${district}, ${city}-${state}.\nPonto de referência ${adjunct}.`;
+    const encodedMessage = `Olá, meu nome é ${name} e vim pelo instagram!\n\n*Nome completo*: ${fullName}\nPlano: ${plan}\nData de vencimento: Dia ${dueDate}\n\nTelefone pra contato: ${phone}\nEndereço: ${street}, número ${number}, ${district}, ${city}-${state}\nPonto de referência ${adjunct}`;
 
     // Retornar um status 200 e uma mensagem personalizada codificada em URL
     reply
         .code(200)
         .send({ 
             message: `https://wa.me/55${sendPhone}?text=${encodeURI(encodedMessage)}`,
-            status: 200
         });
 });
 
