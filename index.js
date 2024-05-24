d436f24e95b20773e364402e7c7b535611122017
gerencia.dev@nbgdigital.com.br <flavioc.buarque@gmail.com>
2024-05-23 23:49:54 -0300

    bold

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ fastify.post('/message', async (request, reply) => {
     const name = splitName[0];
 
     // Codificar a mensagem em URL
-    const encodedMessage = `Olá, meu nome é ${name} e vim pelo instagram!\nNome completo: ${fullName}\nPlano: ${plan}\nData de vencimento: Dia ${dueDate}.\n\nTelefone pra contato: ${phone}\nEndereço: ${street}, número ${number}, ${district}, ${city}-${state}.\nPonto de referência: ${adjunct}.`;
+    const encodedMessage = `Olá, meu nome é ${name} e vim pelo instagram!\n\n*Nome completo*: ${fullName}\nPlano: ${plan}\nData de vencimento: Dia ${dueDate}.\n\nTelefone pra contato: ${phone}\nEndereço: ${street}, número ${number}, ${district}, ${city}-${state}.\nPonto de referência ${adjunct}.`;
 
     // Retornar um status 200 e uma mensagem personalizada codificada em URL
     reply
